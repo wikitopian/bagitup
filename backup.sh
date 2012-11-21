06ed79bd67a9b5a72b8379c17bface4d947608a6
wikitopian <matt@swarmstrategies.com>
2012-11-21 16:17:18 -0500

    Find config file

--- backup.sh
+++ backup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+# Find and read the config
 
+CONFIG_FILE=$HOME/.backuprc
+
+while getopts ":f" opt; do
+    case $opt in
+        f)
+            echo "Custom config file: $2" >&2
+            CONFIG_FILE=$2
+            ;;
+        \?)
+            echo "USAGE: backup.sh [-f CONFIG_FILE]" >&2
+            ;;
+    esac
+done
+
+echo $CONFIG_FILE
